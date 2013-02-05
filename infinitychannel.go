dfc9fbe85108abdac373026615b069b1c233f1c3
Burke Libbey <burke@libbey.me>
2013-02-05 15:47:50 -0600

    Fixed race condition

--- infinitychannel.go
+++ infinitychannel.go
@@ -5,41 +5,50 @@ import (
   "sync"
 )
 
-type Infinitychannel struct {
+type infinitychannel struct {
   send chan interface{}
   recv chan interface{}
+  hasData bool
   storage *list.List
+  mutex sync.Mutex
   cond *sync.Cond
 }
 
 func New() (send, recv chan interface{}) {
   var mutex sync.Mutex
+  var mutex2 sync.Mutex
   cond := sync.NewCond(&mutex)
-  ic := &Infinitychannel{make(chan interface{}), make(chan interface{}), list.New(), cond}
+  ic := &infinitychannel{make(chan interface{}), make(chan interface{}), false, list.New(), mutex2, cond}
   go ic.receiveItems()
   go ic.sendItems()
   return ic.send, ic.recv
 }
 
-func (ic *Infinitychannel) receiveItems() {
+func (ic *infinitychannel) receiveItems() {
   for {
     item := <- ic.recv
+    ic.mutex.Lock()
     ic.storage.PushBack(item)
+    ic.hasData = true
+    ic.mutex.Unlock()
     ic.cond.Signal()
   }
 }
 
-// I think there's still a race condition here, but it's close
-func (ic *Infinitychannel) sendItems() {
+func (ic *infinitychannel) sendItems() {
   for {
     ic.cond.L.Lock()
-    for ic.storage.Front() == nil {
+    for ic.hasData == false {
       ic.cond.Wait()
     }
     ic.cond.L.Unlock()
 
+    ic.mutex.Lock()
     item := ic.storage.Remove(ic.storage.Front())
+    if ic.storage.Len() == 0 {
+      ic.hasData = false
+    }
+    ic.mutex.Unlock()
     ic.send <- item
   }
 }
-
--- infinitychannel_test.go
+++ infinitychannel_test.go
@@ -4,11 +4,9 @@ import "testing"
 
 func TestInfinitychannel(t *testing.T) {
   recv, send := New()
-  go func() {
     for i := 0; i < 100; i++ {
       send <- "omg"
     }
-  }()
   
   for i := 0; i < 100; i++ {
     <- recv
