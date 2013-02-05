41b4587e84f4303f657c0cac11adae18f85d66ce
Burke Libbey <burke@libbey.me>
2013-02-05 15:52:29 -0600

    cleanup and gofmt

--- infinitychannel.go
+++ infinitychannel.go
@@ -1,53 +1,56 @@
 package infinitychannel
 
 import (
-  "container/list"
-  "sync"
+	"container/list"
+	"sync"
 )
 
 type infinitychannel struct {
-  send chan interface{}
-  recv chan interface{}
-  hasData bool
-  storage *list.List
-  mutex sync.Mutex
-  cond *sync.Cond
+	send    chan interface{}
+	recv    chan interface{}
+	hasData bool
+	storage *list.List
+	mutex   sync.Mutex
+	cond    *sync.Cond
 }
 
 func New() (client_send, client_recv chan interface{}) {
-  var mutex, mutex2 sync.Mutex
-  cond := sync.NewCond(&mutex)
-  ic := &infinitychannel{make(chan interface{}), make(chan interface{}), false, list.New(), mutex2, cond}
-  go ic.receiveItems()
-  go ic.sendItems()
-  return ic.recv, ic.send
+	ic := &infinitychannel{}
+	ic.send = make(chan interface{})
+	ic.recv = make(chan interface{})
+	ic.storage = list.New()
+	var mutex sync.Mutex
+	ic.cond = sync.NewCond(&mutex)
+	go ic.receiveItems()
+	go ic.sendItems()
+	return ic.recv, ic.send
 }
 
 func (ic *infinitychannel) receiveItems() {
-  for {
-    item := <- ic.recv
-    ic.mutex.Lock()
-    ic.storage.PushBack(item)
-    ic.hasData = true
-    ic.mutex.Unlock()
-    ic.cond.Signal()
-  }
+	for {
+		item := <-ic.recv
+		ic.mutex.Lock()
+		ic.storage.PushBack(item)
+		ic.hasData = true
+		ic.mutex.Unlock()
+		ic.cond.Signal()
+	}
 }
 
 func (ic *infinitychannel) sendItems() {
-  for {
-    ic.cond.L.Lock()
-    for ic.hasData == false {
-      ic.cond.Wait()
-    }
-    ic.cond.L.Unlock()
+	for {
+		ic.cond.L.Lock()
+		for ic.hasData == false {
+			ic.cond.Wait()
+		}
+		ic.cond.L.Unlock()
 
-    ic.mutex.Lock()
-    item := ic.storage.Remove(ic.storage.Front())
-    if ic.storage.Len() == 0 {
-      ic.hasData = false
-    }
-    ic.mutex.Unlock()
-    ic.send <- item
-  }
+		ic.mutex.Lock()
+		item := ic.storage.Remove(ic.storage.Front())
+		if ic.storage.Len() == 0 {
+			ic.hasData = false
+		}
+		ic.mutex.Unlock()
+		ic.send <- item
+	}
 }
--- infinitychannel_test.go
+++ infinitychannel_test.go
@@ -3,13 +3,13 @@ package infinitychannel
 import "testing"
 
 func TestInfinitychannel(t *testing.T) {
-  send, recv := New()
-    for i := 0; i < 100; i++ {
-      send <- "omg"
-    }
-  
-  for i := 0; i < 100; i++ {
-    <- recv
-    println("omg")
-  }
+	send, recv := New()
+	for i := 0; i < 100; i++ {
+		send <- "omg"
+	}
+
+	for i := 0; i < 100; i++ {
+		<-recv
+		println("omg")
+	}
 }
